97782433a13aa1bd3c9f4bfa979879c099351e8f
Aditya Vikram Godawat <aditya.godawat@gmail.com>
2015-10-19 13:21:43 +0530

    Better Layout

--- app/src/main/java/com/mapplinks/ohmslawcalculator/MainActivity.java
+++ app/src/main/java/com/mapplinks/ohmslawcalculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     TextView  message;
     EditText voltage, current, resistance;
     Button calc, reset;
+
+    String law = "Ohm's Law states that V=I/R\n";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         current = (EditText) findViewById(R.id.current);
         resistance = (EditText) findViewById(R.id.resistance);
 
+        message=(TextView)findViewById(R.id.message);
+
         reset = (Button) findViewById(R.id.reset);
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,10 +40,12 @@ public class MainActivity extends AppCompatActivity {
                 voltage.setText("");
                 current.setText("");
                 resistance.setText("");
+                message.setText("");
+                keyboardHide();
+
             }
         });
 
-        message=(TextView)findViewById(R.id.message);
 
         calc = (Button) findViewById(R.id.calc);
         calc.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 String A = current.getText().toString();
                 String R=resistance.getText().toString();
                 calculate(V, A ,R);
+                keyboardHide();
             }
         });
     }
@@ -61,26 +69,26 @@ void calculate(String V, String A, String R){
         v=Float.parseFloat(V);
         i= Float.parseFloat(A);
         r=v/i;
-        voltage.setText(V + " V");
-        current.setText(A + " A");
-        resistance.setText(Float.toString(r)+ " Ω");
-        message.setText("Voltage: "+V + " V\n"+"Current: "+A + " A\n"+"Resistance: "+Float.toString(r)+ " Ω");
+        voltage.setText(V);
+        current.setText(A);
+        resistance.setText(Float.toString(r));
+        message.setText(law +"Voltage: "+V + " V\n"+"Current: "+A + " A\n"+"Resistance: "+Float.toString(r)+ " Ω");
     }else if(!V.isEmpty() && !R.isEmpty()){
         v=Float.parseFloat(V);
         r= Float.parseFloat(R);
         i=v/r;
-        voltage.setText(V + " V");
-        resistance.setText(R+ " Ω");
-        current.setText(Float.toString(i)+ " A");
-        message.setText("Voltage: "+V + " V\n"+"Current: "+Float.toString(i) + " A\n"+"Resistance: "+R+ " Ω");
+        voltage.setText(V);
+        resistance.setText(R);
+        current.setText(Float.toString(i));
+        message.setText(law+"Voltage: "+V + " V\n"+"Current: "+Float.toString(i) + " A\n"+"Resistance: "+R+ " Ω");
     }else if(!A.isEmpty() && !R.isEmpty()){
         i=Float.parseFloat(A);
         r= Float.parseFloat(R);
         v=i*r;
-        voltage.setText(Float.toString(v)+ " V");
-        resistance.setText(R+ " Ω");
-        current.setText(A+ " A");
-        message.setText("Voltage: "+Float.toString(v) + " V\n"+"Current: "+A + " A\n"+"Resistance: "+R+ " Ω");
+        voltage.setText(Float.toString(v));
+        resistance.setText(R);
+        current.setText(A);
+        message.setText(law+"Voltage: "+Float.toString(v) + " V\n"+"Current: "+A + " A\n"+"Resistance: "+R+ " Ω");
     }else{
         Toast.makeText(MainActivity.this, "Enter the Appropriate Values!", Toast.LENGTH_SHORT).show();
     }
@@ -97,6 +105,15 @@ void calculate(String V, String A, String R){
 //                        .setAction("Action", null).show();
 //            }
 //        });
+
+    void keyboardHide(){
+        try  {
+            InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        } catch (Exception e) {
+
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
